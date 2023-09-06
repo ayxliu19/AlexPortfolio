463a51c931995962c4f494152658327f123f0b25
Alex Liu <arexliu8055@gmail.com>
2023-09-05 23:55:55 -0400

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import React, { useState } from 'react'
 import Sidenav from './components/Sidenav'
 
 function App() {
-  const [nav, setNav] = useState<boolean>(false);
-  const handleNav = () => {
-    setNav(!nav);
-  }
-
+  
   return (
     <div>
-      <Sidenav className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <Sidenav />
     </div>
   )
 }
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -11,8 +11,8 @@ const Sidenav = () => {
             <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99]'/>
             {
                 nav ? (
-                    <div>
-                        <a href="#main">
+                    <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-start z-20">
+                        <a href='#main' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration'>
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
